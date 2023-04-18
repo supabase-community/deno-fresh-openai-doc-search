bbc3efdca12356f064f1046dcb4cb3e9f20caf22
thorwebdev <thor@supabase.io>
2023-04-18 16:12:15 +0800

    feat: add vector-search.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,17 +5,19 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $2 from "./routes/api/vector-search.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $$0 from "./islands/SearchDialog.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/vector-search.ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/SearchDialog.tsx": $$0,
   },
   baseUrl: import.meta.url,
   config,
---
+++ islands/SearchDialog.tsx
@@ -0,0 +1,75 @@
+import { useState } from "preact/hooks";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+import { Button } from "../components/Button.tsx";
+import { SSE } from "sse.js";
+import type { CreateCompletionResponse } from "openai";
+
+export default function SearchDialog() {
+  const [search, setSearch] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [answer, setAnswer] = useState<string>("");
+
+  // @ts-ignore TODO: how to type this?
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+    setAnswer("");
+    setIsLoading(true);
+
+    const eventSource = new SSE(`api/vector-search`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      payload: JSON.stringify({ query: search }),
+    });
+
+    function handleError<T>(err: T) {
+      setIsLoading(false);
+      console.error(err);
+    }
+
+    eventSource.addEventListener("error", handleError);
+    eventSource.addEventListener("message", (e: any) => {
+      try {
+        setIsLoading(false);
+
+        if (e.data === "[DONE]") {
+          return;
+        }
+
+        const completionResponse: CreateCompletionResponse = JSON.parse(e.data);
+        const text = completionResponse.choices[0].text;
+
+        setAnswer((answer) => answer + text);
+      } catch (err) {
+        handleError(err);
+      }
+    });
+
+    eventSource.stream();
+
+    setIsLoading(true);
+  };
+
+  return (
+    <>
+      <div class="flex gap-2 w-full">
+        <form onSubmit={onSubmit}>
+          <input
+            name="search"
+            value={search}
+            // @ts-ignore not sure why complaing
+            onInput={(e) => setSearch(e.target?.value ?? "")}
+            placeholder="Search"
+            disabled={!IS_BROWSER}
+            class={`px-3 py-2 bg-white rounded border(gray-500 2) disabled:(opacity-50 cursor-not-allowed)`}
+          />
+          <Button>Search</Button>
+        </form>
+      </div>
+      <div class="flex gap-2 w-full">
+        <p>{isLoading ? "Loading..." : answer}</p>
+      </div>
+    </>
+  );
+}
