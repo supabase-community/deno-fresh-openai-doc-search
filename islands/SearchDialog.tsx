72b0052cf09332c6896797febc9e038adcb1ee5e
Asher Gomez <ashersaupingomez@gmail.com>
2023-04-22 13:34:55 +1000

    feat: add Preact Signals

--- islands/SearchDialog.tsx
+++ islands/SearchDialog.tsx
@@ -1,37 +1,37 @@
-import { useState } from "preact/hooks";
+import { useSignal } from "@preact/signals";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Button } from "../components/Button.tsx";
 import { SSE } from "sse.js";
 import type { CreateCompletionResponse } from "openai";
 
 export default function SearchDialog() {
-  const [search, setSearch] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [answer, setAnswer] = useState<string>("");
+  const search = useSignal("");
+  const isLoading = useSignal(false);
+  const answer = useSignal("");
 
   // @ts-ignore TODO: how to type this?
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(search);
-    setAnswer("");
-    setIsLoading(true);
+    console.log(search.value);
+    answer.value = "";
+    isLoading.value = true;
 
     const eventSource = new SSE(`api/vector-search`, {
       headers: {
         "Content-Type": "application/json",
       },
-      payload: JSON.stringify({ query: search }),
+      payload: JSON.stringify({ query: search.value }),
     });
 
     function handleError<T>(err: T) {
-      setIsLoading(false);
+      isLoading.value = false;
       console.error(err);
     }
 
     eventSource.addEventListener("error", handleError);
     eventSource.addEventListener("message", (e: any) => {
       try {
-        setIsLoading(false);
+        isLoading.value = false;
 
         if (e.data === "[DONE]") {
           return;
@@ -40,7 +40,7 @@ export default function SearchDialog() {
         const completionResponse: CreateCompletionResponse = JSON.parse(e.data);
         const text = completionResponse.choices[0].text;
 
-        setAnswer((answer) => answer + text);
+        answer.value += text;
       } catch (err) {
         handleError(err);
       }
@@ -48,7 +48,7 @@ export default function SearchDialog() {
 
     eventSource.stream();
 
-    setIsLoading(true);
+    isLoading.value = true;
   };
 
   return (
@@ -59,7 +59,7 @@ export default function SearchDialog() {
             name="search"
             value={search}
             // @ts-ignore not sure why complaing
-            onInput={(e) => setSearch(e.target?.value ?? "")}
+            onInput={(e) => search.value = e.target?.value ?? ""}
             placeholder="Search"
             disabled={!IS_BROWSER}
             class={`px-3 py-2 bg-white rounded border(gray-500 2) disabled:(opacity-50 cursor-not-allowed)`}
@@ -68,7 +68,7 @@ export default function SearchDialog() {
         </form>
       </div>
       <div class="flex gap-2 w-full">
-        <p>{isLoading ? "Loading..." : answer}</p>
+        <p>{isLoading.value ? "Loading..." : answer}</p>
       </div>
     </>
   );
