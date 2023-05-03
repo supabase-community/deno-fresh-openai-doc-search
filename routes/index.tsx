d51f2f88fc7298444b13c55a11982ba1110a03e4
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-03 18:38:59 +1000

    description meta

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <>
       <Head>
         <title>Deno Fresh OpenAI Vector Search</title>
+        <meta
+          name="description"
+          content="Template for building your own custom ChatGPT style doc search powered
+          by Fresh, Deno, OpenAI, and Supabase"
+        />
       </Head>
       <div class="p-4 mx-auto max-w-screen-md font-mono">
         <h1 class="text-3xl mb-4">Deno Fresh OpenAI Vector Search</h1>
